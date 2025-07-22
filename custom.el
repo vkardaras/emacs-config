4116198c93c218a80300aaa1a1c171bd7d483c40
vkardaras <vasiliskardaras@gmail.com>
2025-07-22 18:25:59 +0300

    add support for tree-sitter and pdf

--- custom.el
+++ custom.el
@@ -4,9 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-dired ef-themes evil-collection general hl-todo magit
-                         org-bullets perspective sudo-edit tldr
-                         toc-org vterm-toggle)))
+   '(company consult-lsp dashboard doom-modeline ef-themes embark
+             embark-consult evil-collection flycheck general
+             git-timemachine hl-todo lsp-java lsp-ui magit marginalia
+             nerd-icons-dired orderless org-bullets pdf-tools pdf-view
+             perspective sudo-edit tldr toc-org treemacs-persp
+             treemacs-projectile treesit-auto vertico vterm-toggle
+             yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -4,6 +4,51 @@
 (package-initialize)
 (setq package-check-signature nil)
 
+;;; Sane defaults
+(delete-selection-mode 1)    ;; You can select text and delete it by typing.
+;; (electric-indent-mode -1)    ;; Turn off the weird indenting that Emacs does by default.
+(electric-pair-mode 1)       ;; Turns on automatic parens pairing
+;; The following prevents <> from auto-pairing when electric-pair-mode is on.
+;; Otherwise, org-tempo is broken when you try to <s TAB...
+(require 'org-tempo)
+(add-hook 'org-mode-hook (lambda ()
+           (setq-local electric-pair-inhibit-predicate
+                   `(lambda (c)
+                  (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+(global-auto-revert-mode t)  ;; Automatically show changes if the file has changed
+(global-display-line-numbers-mode 1) ;; Display line numbers
+(menu-bar-mode -1)           ;; Disable the menu bar 
+(scroll-bar-mode -1)         ;; Disable the scroll bar
+(tool-bar-mode -1)           ;; Disable the tool bar
+(recentf-mode 1)
+;; Save what you enter into minibuffer prompts
+(setq history-length 10)
+(savehist-mode 1)
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+(setq org-edit-src-content-indentation 0)
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+;; Revert Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
+;; Tab width
+(setq-default tab-width 4)
+;; General key bindings
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(global-set-key [escape] 'keyboard-escape-quit)
+
+;; add line numbers
+(global-visual-line-mode t)  ;; Enable truncated lines
+(setq display-line-numbers-type 'relative)
+(dolist (mode '(vterm-mode-hook
+                term-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 ;; Add scripts folder
 (add-to-list 'load-path (concat user-emacs-directory "scripts/"))
 ;; Buffer-move for better window management
@@ -16,24 +61,6 @@
 ;; Backup
 (setq backup-directory-alist '((".*" . "~/.local/share/Trash/files")))
 
-;; Sane defaults
-(delete-selection-mode 1)    ;; You can select text and delete it by typing.
-(electric-indent-mode -1)    ;; Turn off the weird indenting that Emacs does by default.
-(electric-pair-mode 1)       ;; Turns on automatic parens pairing
-;; The following prevents <> from auto-pairing when electric-pair-mode is on.
-;; Otherwise, org-tempo is broken when you try to <s TAB...
-(add-hook 'org-mode-hook (lambda ()
-           (setq-local electric-pair-inhibit-predicate
-                   `(lambda (c)
-                  (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
-(global-auto-revert-mode t)  ;; Automatically show changes if the file has changed
-(global-display-line-numbers-mode 1) ;; Display line numbers
-(global-visual-line-mode t)  ;; Enable truncated lines
-(menu-bar-mode -1)           ;; Disable the menu bar 
-(scroll-bar-mode -1)         ;; Disable the scroll bar
-(tool-bar-mode -1)           ;; Disable the tool bar
-(setq org-edit-src-content-indentation 0)
-
 ;; General customization
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message t)
@@ -42,10 +69,7 @@
 (setq initial-major-mode 'org-mode)
 (setq-default indent-tabs-mode nil)
 (setq pop-up-windows nil)
-
-;; add line numbers
-(global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
+(setq use-package-always-ensure t)
 
 ;; Fonts
 (add-to-list 'default-frame-alist '(font . "JetBrains Mono-12"))
@@ -55,19 +79,12 @@
   :slant 'italic)
 (setq-default line-spacing 0.12)
 
-;; General key bindings
-(global-set-key (kbd "C-=") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
-(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
-(global-set-key [escape] 'keyboard-escape-quit)
+;; Modeline
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
-;; Which key
-(which-key-mode 1)
-(which-key-setup-side-window-bottom)
-
-
-;; Complations
+;; Completions
 (use-package completion-preview
   :ensure t
   :config
@@ -77,44 +94,63 @@
 
 (fido-vertical-mode 1)
 
-(setf completion-styles '(basic flex)
-      completion-auto-select t ;; Show completion on first call
-      completion-auto-help 'visible ;; Display *Completions* upon first request
-      completions-format 'one-column ;; Use only one column
-      completions-sort 'historical ;; Order based on minibuffer histor
-      completions-max-height 20 ;; Limit completions to 15 (completions start at line 5)
-      completion-ignore-case t)
+;; (setf completion-styles '(basic flex)
+;;       completion-auto-select t ;; Show completion on first call
+;;       completion-auto-help 'visible ;; Display *Completions* upon first request
+;;       completions-format 'one-column ;; Use only one column
+;;       completions-sort 'historical ;; Order based on minibuffer histor
+;;       completions-max-height 20 ;; Limit completions to 15 (completions start at line 5)
+;;       completion-ignore-case t)
+
+;; (use-package vertico
+;;   :ensure t
+;;   :config
+;;   (setq vertico-cycle t)
+;;   (setq vertico-resize nil)
+;;   (vertico-mode 1))
+
+(use-package marginalia
+  :config
+  (marginalia-mode 1))
+
+(use-package orderless
+  :config
+  (setq completion-styles '(orderless basic)))
+
+(use-package consult)
+
+(use-package embark
+  :bind (("C-." . embark-act)
+         :map minibuffer-local-map
+         ("C-c C-c" . embark-collect)
+         ("C-c C-e" . embark-export)))
+
+(use-package embark-consult)
 
 ;; Themes
 (use-package ef-themes
-  :ensure t
   :config
   (ef-themes-select 'ef-night))
 
-;; All the icons
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p))
-
-(use-package all-the-icons-dired
-  :ensure t
-  :hook (dired-mode . (lambda () (all-the-icons-dired-mode t))))
+;; Nerd icons
+(use-package nerd-icons)
+(use-package nerd-icons-dired
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
 
 ;; Evil
 (use-package evil
-    :ensure t
     :init      ;; tweak evil's configuration before loading it
     (setq evil-want-integration t  ;; This is optional since it's already set to t by default.
           evil-want-keybinding nil
+          evil-want-C-u-scroll t
           evil-vsplit-window-right t
           evil-split-window-below t
-          evil-define-key 'normal vterm-mode-map "p" 'vterm-yank
           evil-undo-system 'undo-redo)  ;; Adds vim-like C-r redo functionality
     (evil-mode 1))
 
 (use-package evil-collection
   :after evil
-  :ensure t
   :config
   (setq evil-collection-mode-list '(dashboard dired ibuffer))
   (evil-collection-init))
@@ -135,23 +171,22 @@
   (general-evil-setup)
 
   ;; set up 'SPC' as the global leader key
-  (general-create-definer dt/leader-keys
+  (general-create-definer vk/leader-keys
     :states '(normal insert visual emacs)
     :keymaps 'override
     :prefix "SPC" ;; set leader
     :global-prefix "M-SPC") ;; access leader in insert mode
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
-  (dt/leader-keys
+  (vk/leader-keys
     ;; "SPC" '(counsel-M-x :wk "Counsel M-x")
     "." '(find-file :wk "Find file")
     "=" '(perspective-map :wk "Perspective") ;; Lists all the perspective keybindings
     "TAB TAB" '(comment-line :wk "Comment lines")
     "u" '(universal-argument :wk "Universal argument"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "b" '(:ignore t :wk "Bookmarks/Buffers")
-    "b b" '(switch-to-buffer :wk "Switch to buffer")
+    "b b" '(consult-buffer :wk "Switch to buffer")
     "b c" '(clone-indirect-buffer :wk "Create indirect buffer copy in a split")
     "b C" '(clone-indirect-buffer-other-window :wk "Clone indirect buffer in new window")
     "b d" '(bookmark-delete :wk "Delete bookmark")
@@ -161,20 +196,23 @@
     "b l" '(list-bookmarks :wk "List bookmarks")
     "b m" '(bookmark-set :wk "Set bookmark")
     "b n" '(next-buffer :wk "Next buffer")
+    "b o" '(consult-outline :wk "Outline document")
     "b p" '(previous-buffer :wk "Previous buffer")
     "b r" '(revert-buffer :wk "Reload buffer")
     "b R" '(rename-buffer :wk "Rename buffer")
     "b s" '(basic-save-buffer :wk "Save buffer")
     "b S" '(save-some-buffers :wk "Save multiple buffers")
     "b w" '(bookmark-save :wk "Save current bookmarks to bookmark file"))
-
-  (dt/leader-keys
+  
+  (vk/leader-keys
     "d" '(:ignore t :wk "Dired")
     "d d" '(dired :wk "Open dired")
-    "d j" '(dired-jump :wk "Dired jump to current"))
+    "d f" '(wdired-finish-edit :wk "Writable dired finish edit")
+    "d j" '(dired-jump :wk "Dired jump to current")
+    "d w" '(wdired-change-to-wdired-mode :wk "Writable dired"))
     ;; "d n" '(neotree-dir :wk "Open directory in neotree")
 
-  (dt/leader-keys
+  (vk/leader-keys
     "e" '(:ignore t :wk "Evaluate")    
     "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
     "e d" '(eval-defun :wk "Evaluate defun containing or after point")
@@ -183,21 +221,25 @@
     "e R" '(eww-reload :which-key "Reload current page in EWW")
     "e w" '(eww :which-key "EWW emacs web wowser"))
   
-  (dt/leader-keys
+  (vk/leader-keys
     "f" '(:ignore t :wk "Files")    
+    ;; Switch to another buffer, or bookmarked file, or recently
+    ;; opened file.
+    "f b" '(consult-buffer :wk "Switch to another buffer")
     "f c" '((lambda () (interactive) (find-file (concat user-emacs-directory "init.el"))) :wk "Edit emacs config")
+    "f d" '(find-grep-dired :wk "Search for string in files in DIR")
     "f e" '((lambda () (interactive)
               (dired user-emacs-directory)) 
             :wk "Open user-emacs-directory in dired")
-    "f d" '(find-grep-dired :wk "Search for string in files in DIR")
-    ;; "f g" '(counsel-grep-or-swiper :wk "Search for string current file")
-    ;; "f j" '(counsel-file-jump :wk "Jump to a file below current directory")
-    ;; "f l" '(counsel-locate :wk "Locate a file")
-    ;; "f r" '(counsel-recentf :wk "Find recent files")
+    "f f" '(consult-find :wk "Search for files names recursively")
+    "f g" '(consult-grep :wk "A recursive grep")
+    "f l" '(consult-line :wk "Search the current buffer")
+    "f o" '(consult-outline :wk "Search through the outline (headings) of the file")
+    "f r" '(consult-recent-file :wk "Find recent files")
     "f u" '(sudo-edit-find-file :wk "Sudo find file")
     "f U" '(sudo-edit :wk "Sudo edit file"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "g" '(:ignore t :wk "Git")    
     "g /" '(magit-displatch :wk "Magit dispatch")
     "g ." '(magit-file-displatch :wk "Magit file dispatch")
@@ -220,7 +262,7 @@
     "g t" '(git-timemachine :wk "Git time machine")
     "g u" '(magit-stage-file :wk "Git unstage file"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "h" '(:ignore t :wk "Help")
     ;; "h a" '(counsel-apropos :wk "Apropos")
     "h b" '(describe-bindings :wk "Describe bindings")
@@ -251,8 +293,11 @@
     "h v" '(describe-variable :wk "Describe variable")
     "h w" '(where-is :wk "Prints keybinding for command if set")
     "h x" '(describe-command :wk "Display full documentation for command"))
+
+  ;; (vk/leader-keys
+  ;;   "l" '(lsp-keymap-prefix :wk "LSP"))
   
-  (dt/leader-keys
+  (vk/leader-keys
     "m" '(:ignore t :wk "Org")
     "m a" '(org-agenda :wk "Org agenda")
     "m e" '(org-export-dispatch :wk "Org export dispatch")
@@ -261,31 +306,29 @@
     "m B" '(org-babel-tangle :wk "Org babel tangle")
     "m T" '(org-todo-list :wk "Org todo list"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "m b" '(:ignore t :wk "Tables")
     "m b -" '(org-table-insert-hline :wk "Insert hline in table"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "m d" '(:ignore t :wk "Date/deadline")
     "m d t" '(org-time-stamp :wk "Org time stamp"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "o" '(:ignore t :wk "Open")
     ;; "o d" '(dashboard-open :wk "Dashboard")
+    "o t" '(treemacs :wk "Open treemacs")
     "o f" '(make-frame :wk "Open buffer in new frame")
     "o F" '(select-frame-by-name :wk "Select frame by name"))
 
-  ;; (dt/leader-keys
-  ;;   "p" '(projectile-command-map :wk "Projectile"))
-
-  (dt/leader-keys
+  (vk/leader-keys
     "s" '(:ignore t :wk "Search")
     "s d" '(dictionary-search :wk "Search dictionary")
     "s m" '(man :wk "Man pages")
     "s t" '(tldr :wk "Lookup TLDR docs for a command")
     "s w" '(woman :wk "Similar to man but doesn't require man"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "t" '(:ignore t :wk "Toggle")
     ;; "t f" '(flycheck-mode :wk "Toggle flycheck")
     "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
@@ -294,7 +337,7 @@
     "t t" '(visual-line-mode :wk "Toggle truncated lines")
     "t v" '(vterm-toggle :wk "Toggle vterm"))
 
-  (dt/leader-keys
+  (vk/leader-keys
     "w" '(:ignore t :wk "Windows")
     ;; Window splits
     "w c" '(evil-window-delete :wk "Close window")
@@ -311,7 +354,11 @@
     "w H" '(buf-move-left :wk "Buffer move left")
     "w J" '(buf-move-down :wk "Buffer move down")
     "w K" '(buf-move-up :wk "Buffer move up")
-    "w L" '(buf-move-right :wk "Buffer move right"))
+    "w L" '(buf-move-right :wk "Buffer move right")
+    ;; Words
+    "w d" '(downcase-word :wk "Downcase word")
+    "w u" '(upcase-word :wk "Upcase word")
+    "w =" '(count-words :wk "Count words/lines for buffer"))
 
 )
 
@@ -328,7 +375,7 @@
 	  which-key-min-display-lines 6
 	  which-key-side-window-slot -10
 	  which-key-side-window-max-height 0.25
-	  which-key-idle-delay 0.8
+	  which-key-idle-delay 0.3
 	  which-key-max-description-length 25
 	  which-key-allow-imprecise-window-fit nil
 	  which-key-separator " → " ))
@@ -361,7 +408,7 @@
 (use-package sudo-edit
   :ensure t
   :config
-    (dt/leader-keys
+    (vk/leader-keys
       "fu" '(sudo-edit-find-file :wk "Sudo find file")
       "fU" '(sudo-edit :wk "Sudo edit file")))
 
@@ -371,6 +418,7 @@
   :config
   (setq shell-file-name "/bin/zsh"
       vterm-max-scrollback 5000))
+(evil-define-key 'normal vterm-mode-map "p" 'vterm-yank)
 
 (use-package vterm-toggle
   :ensure t
@@ -402,9 +450,7 @@
     (evil-define-key 'normal git-timemachine-mode-map (kbd "C-k") 'git-timemachine-show-next-revision)
 )
 ;; Magit
-(use-package magit
-  :ensure t
-  :bind (("C-x g" . magit-status)))
+(use-package magit)
 
 ;; Perspective
 (use-package perspective
@@ -433,10 +479,104 @@
 ;; TLDR
 (use-package tldr :ensure t)
 
-;;; TODO
-;; projectile
-;; dashboard
-;; diminish
-;; flycheck
-;; company
-;; elfeed
+;; Dashboard
+(use-package dashboard
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
+  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
+  (setq dashboard-startup-banner (concat user-emacs-directory "images/emacs-dash.png"))  ;; use custom image as banner
+  (setq dashboard-center-content t) ;; set to 't' for centered content
+  (setq dashboard-display-icons-p t)     ; display icons on both GUI and terminal
+  (setq dashboard-icon-type 'nerd-icons) ; use `nerd-icons' package
+  (setq dashboard-items '((recents . 5)
+                          (agenda . 5 )
+                          (bookmarks . 3)
+                          (projects . 3)
+                          (registers . 3)))
+  :custom 
+  (dashboard-modify-heading-icons '((recents . "file-text")
+				      (bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+(add-hook 'dashboard-mode-hook (lambda () (display-line-numbers-mode -1)))
+
+;;; LSP
+
+;; Project management
+(require 'project)
+;; (use-package project
+;;   :bind-keymap
+;;   (("C-c p" . project-prefix-map)))
+;; treemacs
+(use-package treemacs
+  :config (treemacs-project-follow-mode t))
+(add-hook 'treemacs-mode-hook (lambda() (display-line-numbers-mode -1)))
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package lsp-mode
+  :init (setq lsp-keymap-prefix "C-c l")
+  :bind
+  (:map lsp-mode-map
+        (("\C-\M-b" . lsp-find-implementation)
+         ("M-RET" . lsp-execute-code-action)))
+  :hook ((lsp-mode . lsp-enable-which-key-integration)))
+(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package lsp-ui)
+(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+(use-package dap-java
+  :ensure nil
+  :after (lsp-java)
+
+  :config
+  (global-set-key (kbd "<f7>") 'dap-step-in)
+  (global-set-key (kbd "<f8>") 'dap-next)
+  (global-set-key (kbd "<f9>") 'dap-continue))
+(use-package lsp-treemacs)
+(use-package consult-lsp)
+(add-hook 'compilation-filter-hook
+          (lambda () (ansi-color-apply-on-region (point-min) (point-max))))
+(use-package yasnippet :config (yas-global-mode))
+(require 'treesit)
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
+
+;; Load mode from file type
+(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))
+;; (use-package flycheck)
+;; (use-package hydra)
+
+;; PDF
+(use-package pdf-tools
+  :defer t
+  :commands (pdf-loader-install)
+  :mode "\\.pdf\\'"
+  :bind (:map pdf-view-mode-map
+              ("j" . pdf-view-next-line-or-next-page)
+              ("k" . pdf-view-previous-line-or-previous-page)
+              ("C-=" . pdf-view-enlarge)
+              ("C--" . pdf-view-shrink))
+  :init (pdf-loader-install)
+  :config (add-to-list 'revert-without-query ".pdf"))
+
+(add-hook 'pdf-view-mode-hook #'(lambda () (interactive) (display-line-numbers-mode -1)
+                                                         (blink-cursor-mode -1)
+                                                         (doom-modeline-mode -1)))
+(setq doc-view-continuous t)
+
+;; Ediff
+(setq ediff-split-window-function 'split-window-horizontally
+      ediff-window-setup-function 'ediff-setup-windows-plain)
+
+(defun vk-ediff-hook ()
+  (ediff-setup-keymap)
+  (define-key ediff-mode-map "j" 'ediff-next-difference)
+  (define-key ediff-mode-map "k" 'ediff-previous-difference))
+
+(add-hook 'ediff-mode-hook 'vk-ediff-hook)
